30c5ce91be54eaf666a50cec3b3e763848d001f6
To My <myfragithub@gmail.com>
2020-12-28 13:19:11 +0700

    success add to cart, but not fixed

--- app/Http/Controllers/Web/CartController.php
+++ app/Http/Controllers/Web/CartController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 use App\Models\Cart;
+use App\Models\Product;
+use App\Models\ProductVariant;
 
 class CartController extends Controller
 {
@@ -22,10 +26,72 @@ class CartController extends Controller
 
     public function addToCart(Request $request)
     {
+        $allProductsId = join(',', $this->getAllId(Product::get()));
+        
+        $validator = Validator::make($request->all(), [
+            'product_id' => "required|in:$allProductsId",
+            'variant'    => 'required',
+            'amount'     => 'required|numeric|min:1'
+        ], [
+            'product_id.required' => 'Produk tidak tidak boleh kosong',
+            'product_id.in'       => 'Produk tidak ada / sudah dihapus',
+            'amount.required'     => 'Jumlah tidak boleh kosong',
+            'amount.numeric'      => 'Jumlah harus menggunakan angka',
+            'amount.min'          => 'Jumlah minimal 1',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'code'      => 401,
+                'success'   => (boolean) false,
+                'message'   => "Doesn't pass validation",
+                'data'      => [
+                    'error'  => $validator->errors(),
+                ]
+            ], 200);
+        }
+
+        $product = Product::find($request->product_id);
+        $secondValidator = Validator::make($request->all(), [
+            'amount'     => "max:$product->amount"
+        ], [
+            'amount.max'      => 'Jumlah maksimal adalah ' . $product->amount,
+        ]);
+
+        if($secondValidator->fails()) {
+            return response()->json([
+                'code'      => 401,
+                'success'   => (boolean) false,
+                'message'   => 'Jumlah maksimal adalah ' . $product->amount,
+            ], 200);
+        }
+
+        if( $request->variant['has_variant'] ) {
+            $db_product_variants = ProductVariant::where('product_id', $product->id)->get();
+            $db_product_variants = $this->getAllOneColumn($db_product_variants, 'variant');
+    
+            if( !in_array($request->variant['variant'], $db_product_variants) ) {
+                return response()->json([
+                    'code'      => 404,
+                    'success'   => (boolean) false,
+                    'message'   => "Variant tidak ditemukan",
+                ], 200);
+            }
+        }
+
+
+        $cart = Cart::create([
+            'customer_id'   => Auth::guard('customer')->user()->id,
+            'product_id'    => $product->id,
+            'amount'        => $request->amount,
+            'variant'       => ( $request->variant['has_variant'] ) ? $request->variant['variant'] : '',
+        ]);
+        
         return response()->json([
             'code'      => '200',
             'success'   => (boolean) true,
             'message'   => 'Produk berhasil ditambahkan ke keranjang',
+            'data'      => $cart,
         ], 200);
     }
 }
--- resources/views/web/pages/product/show.blade.php
+++ resources/views/web/pages/product/show.blade.php
@@ -9,7 +9,26 @@
     @include('web.pages.product.components.product-bottom-navigation-order')
 @endsection
 
+@section('stylesheet')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@3/dark.css">
+@endsection
+
 @section('script')
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9/dist/sweetalert2.min.js"></script>
+    <script>
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'center',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+        })
+    </script>
     <script>
         const plus = document.getElementById('plus');
         const min  = document.getElementById('min');
@@ -85,12 +104,34 @@
         add_to_cart.addEventListener('click', () => {
             if( {{ is_null(Auth::guard('customer')->user()) ? 'false' : 'true' }} ) {
                 const url = '{{ url('/add-to-cart') }}';
+
+                if( {{ (count($product->product_variants) > 0) ? 'true' : 'false' }} ) {
+                    const allVariants = document.querySelectorAll('#varian-wrapper .warna');
+                    for (let i = 0; i <= allVariants.length - 1; i++) {
+                        if( allVariants[i].classList.contains('active') ) {
+                            var variant = allVariants[i].querySelector('.text').innerHTML;
+                        }
+                    }
+                }
+
+                if( {{ (count($product->product_variants) > 0) ? 'true' : 'false' }} ) {
+                    if( variant == undefined ) {
+                            Toast.fire({
+                                icon: 'error',
+                                title: 'Pilih Varian terlebih dahulu'
+                            })
+                        return;
+                    }
+                }
                 $data     = {
                     '_token'     : document.getElementById('token').value,
                     'product_id' : {{ $product->id }},
                     'amount'     : parseInt(document.querySelector('input[name=amount]').value),
+                    'variant'    : {
+                        'has_variant'  : {{ (count($product->product_variants) > 0) ? 'true' : 'false' }},
+                        {{ (count($product->product_variants) > 0) ? "variant : variant" : '' }}
+                    }
                 }
-
                 fetch(url, {
                     method : 'POST',
                     headers: {
@@ -99,7 +140,12 @@
                     body : JSON.stringify($data),
                 }).then(response => response.json())
                     .then(res => {
-                        console.log(res);
+                        if(res.code == 200 && res.success) {
+                            Toast.fire({
+                                icon: 'success',
+                                title: res.message
+                            })
+                        }
                     })
 
             } else {
