f32524edfb670528a942c6822ceaedf06bfd3670
To My <myfragithub@gmail.com>
2021-01-25 08:44:43 +0700

    add validator class

--- app/Http/Controllers/Web/PurchasesController.php
+++ app/Http/Controllers/Web/PurchasesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 use App\Models\Sales;
