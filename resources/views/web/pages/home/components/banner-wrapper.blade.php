ed062145fa621e0943c1e3742a42b0d140dad7e3
MyFRA <tomyntapss@gmail.com>
2022-04-18 09:41:53 +0700

    fix image hero

--- resources/views/web/pages/home/components/banner-wrapper.blade.php
+++ resources/views/web/pages/home/components/banner-wrapper.blade.php
@@ -1,5 +1,5 @@
 <div class="banner-wrapper">
-    <div class="banner" style="background-image: url('https://htmldemo.hasthemes.com/boria-preview/boria/assets/img/hero/hero-home-3-img-1.jpg')">
+    <div class="banner" style="background-image: url('images/banner/hero-1.jpg')">
         <div class="title-wrapper">
             <h1>Lorem ipsum dolor sit amet <span>consectetur adipisicing elit</span></h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores repellat ad recusandae quisquam corporis aspernatur error id officiis dolorum voluptatibus.</p>
